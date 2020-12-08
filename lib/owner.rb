69d6f8ddc4f4b61657e9462b31615e6c6730fc91
kimalibennett <aminbennett@gmail.com>
2020-12-08 16:54:18 -0600

    commit tree fc655ed4b01bbdb9f48c0289aef7db287da3b481

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :species, :name 
+  
+  @@all = []
+  
+  def initialize(name)  
+    @name = name
+    @species = "human"
+    @pets = {:dogs => [], :cats => []}
+    @@all << self 
+    @cats = Cat             
+  end 
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear     
+  end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+  end
 end
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+  end
+end
+
+def buy_cat(cat_name)
+  @pets[:cats] << Cat.new(cat_name,self)
+end
+
+def buy_dog(dog_name)
+  @pets[:dogs] << Dog.new(dog_name,self)
+end
+
+def feed_cats
+  self.cats.each do |cat|
+    cats.mood = "happy"
+end
+end
+
+def list_pets
+  @pets
+end
+
+end
+
